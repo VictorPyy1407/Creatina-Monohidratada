eb799727a73a3a877af913f8137d16fbe7f67044
VictorPyy1407 <113316217+VictorPyy1407@users.noreply.github.com>
2026-07-04 01:26:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+window.APP_CONFIG = {
+  PRODUCT_NAME: 'Creatina Monohidratada GECAPS 300g',
+  PRODUCT_PRICE: 149000,
+  PRODUCT_OLD_PRICE: 220000,
+  CURRENCY: 'PYG',
+  CURRENCY_SYMBOL: 'Gs.',
+  GA4_ID: 'G-8WM6CYEB73',
+  GTM_ID: 'GTM-MWP3LRKX',
+  META_PIXEL_ID: '2412226475899711',
+  SUPABASE_URL: 'https://roruinqorwgolcrhhmpm.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcnVpbnFvcndnb2xjcmhobXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTU0MDcsImV4cCI6MjA5ODIzMTQwN30.VzNSqYUM6amTOToZUsJ7Emjapy-y9Y44hDmbC1XG9Eg',
+  SUPABASE_TABLE: 'cleanpro',
+  ORIGIN: 'landing_creatina_monohidratada_gecaps_300g',
+  WHATSAPP_NUMBER: '595972738779',
+  WHATSAPP_MESSAGE: 'Hola! Me interesa la Creatina Monohidratada GECAPS 300g',
+  COMBOS: {
+    1: 149000,
+    2: 235000,
+    3: 295000
+  }
+};
